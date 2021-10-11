4ffd3c733de2d6cb9ae3752caa09808b89e9fbec
zhangyufan <zhangyufan@comisys.net>
2021-10-11 09:43:01 +0800

    新增call apply promise

--- 算法练习/call.js
+++ 算法练习/call.js
@@ -1,18 +1,16 @@
 // 思路：将要改变this指向的方法挂到目标this上执行并返回
-Function.prototype.mycall = function (context) {
+Function.prototype.mycall = function (context = window) {
   if (typeof this !== 'function') {
     throw new TypeError('not funciton');
   }
   context = context || window;
   context.fn = this;
-  console.log(this);
   let arg = [...arguments].slice(1);
   let result = context.fn(...arg);
-  console.log(context.fn);
   delete context.fn;
   return result;
 };
 function foo() {}
 function foo2() {}
 const obj = {};
-foo.mycall(foo2);
+foo.mycall(foo2, 1, 2, 3, 4);
--- 算法练习/test.cpp
+++
@@ -1,9 +0,0 @@
-int result[3] = {0, 1, 1};
-int Fibonacci1(unsigned n)
-{
-    if (n < 3)
-    {
-        return result[n];
-    }
-    return Fibonacci1(n - 1) + Fibonacci1(n - 2);
-}
